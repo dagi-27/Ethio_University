41ac783ee90eb5dc93716d21a9d8eb0fdf69fab4
Mintesnot Teshome <126505475+mintesnot-teshome@users.noreply.github.com>
2025-03-11 12:38:55 +0300

    Update top-universities.php

--- top-universities.php
+++ top-universities.php
@@ -1,1 +1,56 @@
+<?php
+      <section class="top-universities">
+        <div class="section-header">
+            <h2>Top Ethiopian Universities</h2>
+            <a href="ranking.php" class="view-all-btn">View All Rankings</a>
+        </div>
+        <p>Below, you'll find a selection of leading institutions in Ethiopia. Click on a university to access detailed information.</p>
 
+        <div class="university-list">
+          <div class="university-item">
+            <img src="./image/addis_main.png" alt="Addis Ababa University" />
+            <div class="university-info">
+              <span class="rank">#1</span>
+              <a href="universities/university-details.php?id=addis-ababa"
+                >Addis Ababa University</a
+              >
+            </div>
+          </div>
+          <div class="university-item">
+            <img src="./image/jimma_main.png" alt="Jimma University" />
+            <div class="university-info">
+              <span class="rank">#2</span>
+              <a href="universities/university-details.php?id=jimma"
+                >Jimma University</a
+              >
+            </div>
+          </div>
+          <div class="university-item">
+            <img src="./image/bahir-dar.png" alt="Bahir Dar University" />
+            <div class="university-info">
+              <span class="rank">#3</span>
+              <a href="universities/university-details.php?id=bahir-dar"
+                >Bahir Dar University</a
+              >
+            </div>
+          </div>
+          <div class="university-item">
+            <img src="./image/mekelle.png" alt="Mekelle University" />
+            <div class="university-info">
+              <span class="rank">#4</span>
+              <a href="universities/university-details.php?id=mekelle"
+                >Mekelle University</a
+              >
+            </div>
+          </div>
+          <div class="university-item">
+            <img src="./image/hawassa.png" alt="Hawassa University" />
+            <div class="university-info">
+              <span class="rank">#5</span>
+              <a href="universities/university-details.php?id=hawassa"
+                >Hawassa University</a
+              >
+            </div>
+          </div>
+        </div>
+      </section>
